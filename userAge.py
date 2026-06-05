2daff97334c8181340f07645bced22fcbebbd2ed
MASKY-33 <masky.somebody33@gmail.com>
2026-06-06 01:04:32 +0200

    Create userAge.py

---
+++ userAge.py
@@ -0,0 +1,10 @@
+userAge = int(input("Enter your age: "))
+
+if userAge <= 12:
+    print("You are too young to start a business. Enjoy your childhood!")
+elif userAge <= 18:
+    print ("You are old enough. You can start to build a business, focus on self-improvement, and building up yourself.")
+elif userAge <= 35:
+    print("You have to start planning building a business now! Otherwise, you will have regret for the rest of your life for not starting and trying!")
+else:
+    print("You are too old to start a business. You should focus on enjoying your life and spending time with your loved ones!")
